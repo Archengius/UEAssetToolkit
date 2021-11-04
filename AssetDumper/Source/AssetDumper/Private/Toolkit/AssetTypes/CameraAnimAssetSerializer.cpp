21ab7f8f6b928385ce213ed2e2f5a0e9a87e8125
Archengius <Archengius@gmail.com>
2021-11-05 06:11:31 +0700

    Camera Anim asset dumping

---
+++ AssetDumper/Source/AssetDumper/Private/Toolkit/AssetTypes/CameraAnimAssetSerializer.cpp
@@ -0,0 +1,15 @@
+﻿#include "Toolkit/AssetTypes/CameraAnimAssetSerializer.h"
+#include "Toolkit/AssetDumping/AssetTypeSerializerMacros.h"
+#include "Toolkit/ObjectHierarchySerializer.h"
+#include "Toolkit/AssetDumping/SerializationContext.h"
+#include "Camera/CameraAnim.h"
+
+void UCameraAnimAssetSerializer::SerializeAsset(TSharedRef<FSerializationContext> Context) const {
+	BEGIN_ASSET_SERIALIZATION(UCameraAnim)
+	SERIALIZE_ASSET_OBJECT
+	END_ASSET_SERIALIZATION
+}
+
+FName UCameraAnimAssetSerializer::GetAssetClass() const {
+	return UCameraAnim::StaticClass()->GetFName();
+}
